2f08d47da8fdd77704188c092296969a5bf839c8
ty fejerang <fejerang.ty@gmail.com>
2018-01-21 12:17:08 -0800

    in this commit i finished the last 2 problems of the problem set by creating functions that added numbers of the array and by crating a function that compared the largest number of an array

--- js/index.js
+++ js/index.js
@@ -8,44 +8,84 @@ console.log(array1);
 //   array1.forEach(function(item){
 //     console.log(item)
 //   });
-function printReverse() {
-  for(var i=array1.length;i>= 0; i--){
-    console.log(array1[i]);
+function printReverse(array) {
+  for(var i=array.length;i>= 0; i--){
+    console.log(array[i]);
   }
 };
 printReverse(array1);
 
 console.log("******** answer #1 above ***********")
-// print the array in border
-// reverse the order
+
 
 console.log("Write a function 'isUniform()' which takes an array as an arguemnt and returns true if all elements of the array are identical.")
 console.log("Q # 2 ****************");
 var array2 = [1, 1, 1, 1];
 var array3 = [1, 1, 1, 2];
-var array4 = [2, 2, 2, 2];
-var array5 = [2, 2, 3, 3];
-
-function isUniform() {
-  var result = ""[0];
-  for(var i=0;i<="".length;i++)
-  if(i === result){
-    console.log("true")
-  }
+var array4 = ["b", "b", "b", "b"];
+var array5 =[ "a", "a", "b", "c"];
+// declare variable
+function isUniform(arr){
+// store variable
+var result = arr[0];
+// loop through array
+ for(var i=0;i<=arr.length;i++){
+// compare values to each number int he array
+    if(arr[i]!==result){
+      return "false"
+    };
+  };
+  // if true return true
+  return "true";
 };
 isUniform(array2);
-// save value of array at 0;
-// loop through the array
-// compare numbers to saved value
-// print to console
+isUniform(array3);
+isUniform(array4);
+isUniform(array5);
 
 console.log("******** answer #2 above ***********")
-
+//
 console.log("write function ' sumArray()' that accepts an array of numbers and returns the sum of all numbers.");
 console.log(" Q # 3 *****************");
+var seven = [1, 2, 3, 4];
+var eight = [11, 22, 33];
+var nine = [99, 100, 1];
+var ten = [12, 34, 56, 78];
 
-console.log("******** answer #3 above ***********")
+function sumArray(arr) {
+  // declare variable to store sum
+  var sum =0;
+  // forEach function to loop through every variable
+  arr.forEach(function(i){
+    // with the for each loop add value at "i" to "sum"
+  sum += i;
+});
+// return the sum after the function finishes
+return sum;
+};
+sumArray(seven);
+sumArray(eight);
+sumArray(nine);
+sumArray(ten);
 
+
+console.log("******** answer #3 above ***********");
+//
 console.log(" write a function 'max()' that accepts an array of numbers and returns the largest number in the array.");
 console.log(" Q # 4 *****************");
-console.log("******** answer #4 above ***********")
+// // console.log("******** answer #4 above ***********");
+
+function max(arr) {
+  // create variable to store initial value of the array
+  var max = arr[0];
+  // loop through the array after the initial valur
+  for (var i = 1; i<=arr.length;i++) {
+    // create an if statement to see if the value of i is greater than the current value of max
+    if(arr[i] > max) {
+      // tell the console if the statement is true change the value of max to the current  value at "i" === "arr{i}"
+      var max = arr[i];
+    }
+  }
+  // after the cycle is through return the updated max value
+  return max;
+};
